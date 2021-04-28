de5d3ebdf2d64f600a29a2f91f1c9e2d04cfef9d
Lee Smith <administrator@getaddress.io>
2021-04-28 14:45:05 +0100

    Add project files.

---
+++ GetAddress.Sdk/GetAddressApi.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Net.Http;
+
+namespace GetAddress.Sdk
+{
+    public class GetAddressApi
+    {
+        private readonly HttpClient httpClient;
+        
+        public GetAddressApi(string apiKey, string administrationKey, HttpClient httpClient = null)
+        {
+            ApiKey = apiKey;
+            AdministrationKey = administrationKey;
+            this.httpClient = httpClient ?? GetHttpClient();
+        }
+
+        private HttpClient GetHttpClient()
+        {
+            var client = new HttpClient();
+            client.BaseAddress = BaseAddress;
+
+            return client;
+        }
+
+        public Uri BaseAddress
+        {
+            get;
+            set;
+        } = new Uri("https://api.getaddress.io");
+
+        public string ApiKey { get; set; }
+        public string AdministrationKey { get; set; }
+    }
+}
