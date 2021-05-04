e38f860d21eff922c824cb11494cbe5eb8ff8828
Lee Smith <LeePaulSmith@googlemail.com>
2021-05-04 15:42:03 +0100

    Usage V4 (#7)
    Co-authored-by: Lee Smith <administrator@getaddress.io>

---
+++ GetAddress.Sdk.Tests/UsageTests.cs
@@ -0,0 +1,45 @@
+﻿using Shouldly;
+using System;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace GetAddress.Sdk.Tests
+{
+    public class UsageTests
+    {
+        [Fact]
+        public async Task Usage_Returns_Successful_Result()
+        {
+            var api = Helpers.ApiHelper.GetApi();
+
+            var result = await api.Usage();
+
+            result.IsSuccess.ShouldBeTrue();
+        }
+
+        [Fact]
+        public async Task Given_Date_Usage_Returns_Successful_Result()
+        {
+            var api = Helpers.ApiHelper.GetApi();
+
+            var date = new DateTimeOffset(2021, 05, 04, 14, 15, 1, TimeSpan.FromHours(1));
+
+            var result = await api.Usage(date);
+
+            result.IsSuccess.ShouldBeTrue();
+        }
+
+        [Fact]
+        public async Task Given_From_Date_And_To_Date_Usage_Returns_Successful_Result()
+        {
+            var api = Helpers.ApiHelper.GetApi();
+
+            var fromDate = new DateTimeOffset(2021, 05, 01, 14, 15, 1, TimeSpan.FromHours(1));
+            var toDate = new DateTimeOffset(2021, 05, 04, 14, 15, 1, TimeSpan.FromHours(1));
+
+            var result = await api.Usage(fromDate, toDate);
+
+            result.IsSuccess.ShouldBeTrue();
+        }
+    }
+}
--- GetAddress.Sdk/Services/UsageService.cs
+++ GetAddress.Sdk/Services/UsageService.cs
@@ -1,4 +1,5 @@
-﻿using System.Net.Http;
+﻿using System;
+using System.Net.Http;
 using System.Threading;
 using System.Threading.Tasks;
 
@@ -6,7 +7,7 @@ namespace GetAddress.Sdk.Services
 {
     public class UsageService : AdministrationService
     {
-        private const string path = "v3/usage";
+        private const string path = "v4/usage";
 
         public UsageService(string administrationKey, HttpClient httpClient = null) : base(administrationKey, httpClient)
         {
@@ -21,6 +22,42 @@ namespace GetAddress.Sdk.Services
                 token: accessToken, cancellationToken: cancellationToken);
 
             return await response.ToResult<Usage>();
+        }
+
+        public async Task<Result<Usage>> Get(DateTimeOffset date, AccessToken accessToken = default, CancellationToken cancellationToken = default)
+        {
+            var year = date.UtcDateTime.Year;
+            var month = date.UtcDateTime.Month;
+            var day = date.UtcDateTime.Day;
+
+            var usagePath = $"{path}/{day}/{month}/{year}";
+
+            var requestUri = GetUri(usagePath);
+
+            var response = await HttpGet(requestUri, administrationOrApiKey: AdministrationKey,
+                token: accessToken, cancellationToken: cancellationToken);
+
+            return await response.ToResult<Usage>();
+        }
+
+        public async Task<Result<Usage[]>> Get(DateTimeOffset from, DateTimeOffset to, AccessToken accessToken = default, CancellationToken cancellationToken = default)
+        {
+            var fromYear = from.UtcDateTime.Year;
+            var fromMonth = from.UtcDateTime.Month;
+            var fromDay = from.UtcDateTime.Day;
+
+            var toYear = to.UtcDateTime.Year;
+            var toMonth = to.UtcDateTime.Month;
+            var today = to.UtcDateTime.Day;
+
+            var usagePath = $"{path}/from/{fromDay}/{fromMonth}/{fromYear}/to/{today}/{toMonth}/{toYear}";
+
+            var requestUri = GetUri(usagePath);
+
+            var response = await HttpGet(requestUri, administrationOrApiKey: AdministrationKey,
+                token: accessToken, cancellationToken: cancellationToken);
+
+            return await response.ToResult<Usage[]>();
         }
     }
 }
