dd9268fe3c84ba36819b32abbdf4d3c3a165bd0f
Lee Smith <administrator@getaddress.io>
2021-05-04 19:24:47 +0100

    expired email service and tests

---
+++ GetAddress.Sdk/Results/SuccessfulExpiredEmailRecipient.cs
@@ -0,0 +1,13 @@
+﻿using Newtonsoft.Json;
+
+namespace GetAddress.Sdk
+{
+    public class SuccessfulExpiredEmailRecipient
+    {
+        [JsonProperty("id")]
+        public string Id { get; set; }
+
+        [JsonProperty("email-address")]
+        public string EmailAddress { get; set; }
+    }
+}
---
+++ GetAddress.Sdk/Services/ExpiredEmailRecipientService.cs
@@ -0,0 +1,70 @@
+﻿using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace GetAddress.Sdk.Services
+{
+    public class ExpiredEmailRecipientService: AdministrationService
+    {
+        private const string path = "cc/expired";
+
+        public ExpiredEmailRecipientService(string administrationKey, HttpClient httpClient = null) : base(administrationKey, httpClient)
+        {
+
+        }
+
+        public async Task<Result<SuccessfulExpiredEmailRecipient>> Get(string id,
+            AccessToken accessToken = default,
+            CancellationToken cancellationToken = default)
+        {
+            var removePath = path + $"/{id}";
+
+            var requestUri = GetUri(removePath);
+
+            var response = await HttpGet(requestUri, administrationOrApiKey: AdministrationKey,
+                token: accessToken, cancellationToken: cancellationToken);
+
+            return await response.ToResult<SuccessfulExpiredEmailRecipient>();
+        }
+
+        public async Task<Result<SuccessfulExpiredEmailRecipient[]>> Get(
+            AccessToken accessToken = default,
+            CancellationToken cancellationToken = default)
+        {
+            var requestUri = GetUri(path);
+
+            var response = await HttpGet(requestUri, administrationOrApiKey: AdministrationKey,
+                token: accessToken, cancellationToken: cancellationToken);
+
+            return await response.ToResult<SuccessfulExpiredEmailRecipient[]>();
+        }
+
+        public async Task<Result<SuccessfulExpiredEmailRecipientAdd>> Add(AddExpiredEmailRecipientRequest request,
+            AccessToken accessToken = default,
+            CancellationToken cancellationToken = default)
+        {
+            var requestUri = GetUri(path);
+
+            var content = request.ToHttpContent();
+
+            var response = await HttpPost(requestUri, httpContent: content, administrationOrApiKey: AdministrationKey,
+                token: accessToken, cancellationToken: cancellationToken);
+
+            return await response.ToResult<SuccessfulExpiredEmailRecipientAdd>();
+        }
+
+        public async Task<Result<SuccessfulExpiredEmailRecipientRemove>> Remove(string id,
+            AccessToken accessToken = default,
+            CancellationToken cancellationToken = default)
+        {
+            var removePath = path + $"/{id}";
+
+            var requestUri = GetUri(removePath);
+
+            var response = await HttpDelete(requestUri, administrationOrApiKey: AdministrationKey,
+                token: accessToken, cancellationToken: cancellationToken);
+
+            return await response.ToResult<SuccessfulExpiredEmailRecipientRemove>();
+        }
+    }
+}
