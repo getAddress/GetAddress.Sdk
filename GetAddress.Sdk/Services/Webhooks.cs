4daa5caae1b0aafd3f4f9cb75d4f7f77bd35968c
Lee Smith <administrator@getaddress.io>
2021-05-22 16:06:09 +0100

    Suggest and track webhook service accessible via webhooks class

--- GetAddress.Sdk/Services/Webhooks.cs
+++ GetAddress.Sdk/Services/Webhooks.cs
@@ -24,12 +24,24 @@ namespace GetAddress.Services
             get;
         }
 
+        public TrackWebhookService Track
+        {
+            get;
+        }
+
+        public SuggestLimitReachedWebhookService SuggestLimitReached
+        {
+            get;
+        }
+
         public Webhooks(AdministrationKey administrationKey, HttpClient httpClient = null)
         {
             DailyLimitReached = new DailyLimitedReachedWebhookService(administrationKey, httpClient: httpClient);
             MonthlyReserveReached = new MonthlyReserveReachedWebhookService(administrationKey, httpClient: httpClient);
             PaymentFailed = new PaymentFailedWebhookService(administrationKey, httpClient: httpClient);
             Expired = new ExpiredWebhookService(administrationKey, httpClient: httpClient);
+            Track = new TrackWebhookService(administrationKey, httpClient: httpClient);
+            SuggestLimitReached = new SuggestLimitReachedWebhookService(administrationKey, httpClient: httpClient);
         }
     }
 }
